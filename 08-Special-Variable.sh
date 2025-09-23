08a7be3142bdf8a6fd5c3186caf5a6e77344e722
althaf <shaikalthaf1984@gmail.com>
2025-09-23 18:32:52 +0530

    variable

--- 08-Special-Variable.sh
+++ 08-Special-Variable.sh
@@ -2,9 +2,9 @@
 
 echo "who is PWD: $PWD"
 echo "Home Directory: $HOME"
-echo "Present PID: $PID"
+
 echo "User Name is: $USER"
 echo "Script name is: $0"
 
-sleep 10
-echo "PID of last command in background: $!" 
+
+echo "PID of last command in background: $@" 
