d5af83a0b48f76548da7779ce1e12c9158cb02c8
althaf <shaikalthaf1984@gmail.com>
2025-09-23 16:48:53 +0530

    variable

--- 03-Variable.sh
+++ 03-Variable.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "Trump :: welcome to Alaska Putin"
-echo "Putin :: Thank uyou Trump, finally not on zoom webiner"
-echo  "Trump :: so, do we have a deal"
-echo "Putin :: yes, if you make ukrain joins Russia"
+Person1=$1
+Person2=$2
+echo "$Person1 :: welcome to Alaska $Person2"
+echo "$Person2 :: Thank uyou Person1, finally not on zoom webiner"
+echo "$Person1 :: so, do we have a deal"
+echo "$Peerson2 :: yes, if you make ukrain joins Russia"
