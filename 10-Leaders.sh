bb38d57b39fe70f6ab04091de1012ba1f4adb7d1
althaf <shaikalthaf1984@gmail.com>
2025-09-23 19:01:23 +0530

    variable

--- 10-Leaders.sh
+++ 10-Leaders.sh
@@ -5,4 +5,4 @@ LEADERS=("Modi" "Putin" "Trudo" "Trump")
 echo "All leaders: ${LEADERS[@]}"
 
 echo "First leader: ${LEADERS[0]}"
-echo "Second leader: ${LEADER[1]}"
+echo "Second leader: ${LEADERS[1]}"
