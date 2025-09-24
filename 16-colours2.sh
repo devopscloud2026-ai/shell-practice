4dc7ca02672ff956edeeb101958d0fd92d47eaa9
althaf <shaikalthaf1984@gmail.com>
2025-09-24 22:24:51 +0530

    condition

---
+++ 16-colours2.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="e\[31m"
+G="e\[32m"
+Y="e\[33m"
+N="e\[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root Privelege"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+    echo -e "Installing $2...$R is failure $N"
+    exit 1
+    else
+    echo -e "Installing $2...$G success $N"
+    fi
+    
+}
+    dnf list installed mysql
+    if [ $? -ne 0 ]; then
+   dnf install mysql -y
+   VALIDATE $? "MYSQL"
+   else
+    echo "Already Mysql exist....$G SKIPPING $N"
+
+    dnf list installed nginx
+    if [$? -ne 0 ]; then
+    dnf install nginx -y
+    VALIDATE $? "Nginx"
+    else
+     echo "Already Nginx exist....$G SKIPPING $N"
+
+    dnf list installed python3
+    if [$? -ne 0 ]; then
+    dnf install python3 -y
+    VALIDATE $? "python"
+    else
+    echo "Already python3 exist....$G SKIPPING $N"
