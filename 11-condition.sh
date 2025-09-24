090888ac369dd68482fe32afe36b7922cd4bae41
althaf <shaikalthaf1984@gmail.com>
2025-09-24 19:14:04 +0530

    condition

--- 11-condition.sh
+++ 11-condition.sh
@@ -3,10 +3,10 @@
 NUMBER=$1
 
 if [$NUMBER -lt 10]; then
-    echo "Given number $NUMBER is greather than 10"
+    echo "Given number $NUMBER is lessthan 10"
 
 else
-    echo "Given number $NUMBER is lessthan or equal to 10"
+    echo "Given number $NUMBER is greaterthan or equal to 10"
 fi
 
  
