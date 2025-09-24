a56995b0c4d11d667d0dd02bb61e16bdfd13a05d
althaf <shaikalthaf1984@gmail.com>
2025-09-24 20:53:20 +0530

    condition

---
+++ 13-installation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0]; then
+    echo "ERROR:: Please run this script with root Privelege"
+
+fi
+
+    dnf install MYSQL -y
+
+if [ $! -ne 0 ]; then
+    echo :ERROR:: Installing MYSQL is failure"
+
+else
+    echo "Installing MYSQL is success"
+
+fi
