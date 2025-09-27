c3f8f1c1b6080e3dbf260a12ff1d114ea9480cca
althaf <shaikalthaf1984@gmail.com>
2025-09-27 20:12:19 +0530

    althaf

--- althaf-even-odd.sh
+++ althaf-even-odd.sh
@@ -3,7 +3,7 @@
 echo "Please enter the number"
 read Number
 
-if [ $(($NUMBER % 2 -eq 0)) ]; then
+if [ $(($NUMBER % 2)) -eq 0 ]; then
     echo "Given number $NUMBER is even"
 else
     echo "Given number $NUMBER is odd"
