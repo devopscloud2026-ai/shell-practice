a32e5c73f365b5f1e605d9616c64a62dbbd412bb
althaf <shaikalthaf1984@gmail.com>
2025-09-27 17:07:24 +0530

    althaf

--- althaf-datatpyes9.sh
+++ althaf-datatpyes9.sh
@@ -10,4 +10,5 @@ echo "SUM is: ${SUM}"
 LEADERS=("MODI" "PUTIN" "TRUMP")
 
 echo "All leaders: ${LEADERS[@]}"
-echo "First leader: ${LEADERS[0]}"
+## echo "First leader: ${LEADERS[0]}"
+echo "Second leader: ${LEADERS[1]}"
