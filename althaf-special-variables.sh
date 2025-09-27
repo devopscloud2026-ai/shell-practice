fe8f9db9b99cccd7a10dbf4ed0f27e305c2f2798
althaf <shaikalthaf1984@gmail.com>
2025-09-27 16:14:11 +0530

    althaf

--- althaf-special-variables.sh
+++ althaf-special-variables.sh
@@ -4,6 +4,6 @@ echo "All variables passed in the script: $@"
 echo "All variables passed in the script: $*"
 echo "Home Directory is: $HOME"
 echo "Who is user: $USER"
-echo "what is Pwd: $PWD
+echo "what is Pwd: $PWD"
 sleep 50 &
 echo "what is last backgroupd of PID: $!"
