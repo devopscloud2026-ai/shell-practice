07bc8064be3f7e9c0dce9ff4e8ea4ae4b5f7be44
althaf <shaikalthaf1984@gmail.com>
2025-09-27 19:32:32 +0530

    althaf

---
+++ althaf-condition.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if [ $NUMBER -lt 10 ]; then
+    echo "Given number is less than 10"
+else
+    echo "Given number is greater than or equal to 10"
+fi
--- althaf-datatpyes9.sh
+++ althaf-datatpyes9.sh
@@ -9,6 +9,6 @@ echo "SUM is: ${SUM}"
 
 LEADERS=("MODI" "PUTIN" "TRUMP")
 
-# echo "All leaders: ${LEADERS[@]}"
-## echo "First leader: ${LEADERS[0]}"
+echo "All leaders: ${LEADERS[@]}"
+echo "First leader: ${LEADERS[0]}"
 echo "Second leader: ${LEADERS[1]}"
