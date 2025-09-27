57a39d15043a3869990edaf5ef6434fb0470b29d
althaf <shaikalthaf1984@gmail.com>
2025-09-27 11:39:47 +0530

    althaf

--- althaf-variable3.sh
+++ althaf-variable3.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+PERSON1=$1
+PERSON2=$2
 
-echo "Trump :: Welcome to Putin"
-echo "Putin :: Thank you Trump, finally not on Zoom"
-echo "Trump :: so do we have a deal"
-echo "Putin :: yes, if you make Ukrain joins Russia"
+echo "$PERSON1 :: Welcome to $PERSON2"
+echo "$PERSON2 :: Thank you $PERSON1, finally not on Zoom"
+echo "$PERSON1 :: so do we have a deal"
+echo "$PERSON2 :: yes, if you make Ukrain joins Russia"
