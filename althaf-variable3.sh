44b125934f4270c8df1e33c59064f96af5dc29f0
althaf <shaikalthaf1984@gmail.com>
2025-09-27 11:37:02 +0530

    althaf

---
+++ althaf-variable3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+echo "Trump :: Welcome to Putin"
+echo "Putin :: Thank you Trump, finally not on Zoom"
+echo "Trump :: so do we have a deal"
+echo "Putin :: yes, if you make Ukrain joins Russia"
