ebbfbd9701e4ebbbc452dd5f2db1d18af8f4a970
althaf <shaikalthaf1984@gmail.com>
2025-09-28 08:00:40 +0530

    althaf

--- althaf-installing.sh
+++ althaf-installing.sh
@@ -3,6 +3,15 @@
 USER_ID=$(id -u)
 
 if [ $USER_ID -ne 0 ]; then
-    echo "ERROR :: Please run this with root privelege"
+    echo "ERROR :: Please run this script with root privelege"
 
 fi 
+
+    dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "ERROR :: Installing mysql is failure"
+
+else
+    echo "Installing mysql is success"
+fi
