a1f5651de4d92424a226cb242c54f6ff49e208d6
althaf <shaikalthaf1984@gmail.com>
2025-09-28 17:10:27 +0530

    logs

--- for-loop.sh
+++ for-loop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-USER_ID=$(id -u)
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
@@ -8,26 +8,28 @@ N="\e[0m"
 
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
-    mkdir -p $LOGS_FOLDER
-    echo "script started excuted is: $(date)"
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
-if [ $USER_ID -ne 0 ]; then
-    echo "ERROR :: Please run this script with root privelege"
-    exit 1
-fi 
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
 
-VALIDATE(){
+VALIDATE(){ # functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
-        echo -e "Installing $2...$R is failure $N"
+        echo -e "Installing $2 ... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
     else
-        echo -e "Installing $2...$G success $N"
+        echo -e "Installing $2 ... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
-    
 }
-       for package in $@
+
+# $@
+
+for package in $@
 do
     # check package is already installed or not
     dnf list installed $package &>>$LOG_FILE
@@ -40,5 +42,3 @@ do
         echo -e "$package already installed ... $Y SKIPPING $N"
     fi
 done
-
-    
