302e8e440ebdf03e18ea0b197b14d57bdff1e2f2
althaf <shaikalthaf1984@gmail.com>
2025-09-28 16:47:24 +0530

    logs

---
+++ for-loop.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+    mkdir -p $LOGS_FOLDER
+    echo "script started excuted is: $(date)"
+
+if [ $USER_ID -ne 0 ]; then
+    echo "ERROR :: Please run this script with root privelege"
+    exit 1
+fi 
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "Installing $2...$R is failure $N"
+        exit 1
+    else
+        echo -e "Installing $2...$G success $N"
+    fi
+    
+}
+       for Package in $@
+       do
+       echo "Package is: $package"
+       done
+
+
+    
